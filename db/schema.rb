c8d8dcd00142d18a2d8eb3ce1dc8817118a5f83a
Anton Dydo <dydo00@bk.ru>
2021-11-17 00:57:26 +0300

    to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,21 +12,24 @@
 
 ActiveRecord::Schema.define(version: 2021_11_14_172116) do
 
-  create_table "reviews", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.string "category"
     t.text "description"
     t.string "img_url_1"
     t.string "img_url_2"
     t.string "img_url_3"
-    t.decimal "stars", precision: 10
+    t.decimal "stars"
     t.integer "authors_grade"
     t.integer "stars_amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
