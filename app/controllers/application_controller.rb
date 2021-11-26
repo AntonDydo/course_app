044f927c24e37e55ac672264984ac1333a13db8d
Anton Dydo <dydo00@bk.ru>
2021-11-26 15:37:44 +0300

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+
+  around_action :switch_locale
+  before_action :authenticate_user!
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def set_cookie
+    cookies[:locale] = { value: params[:locale], expires: Time.now + 3000000 }
+    cookies[:theme] = { value: params[:theme], expires: Time.now + 3000000 }
+
+    redirect_back fallback_location: root_path
+    
+  end
+
+  private
+
+  def switch_locale(&block)
+    locale = cookies[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &block)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  root "reviews#index"
-  devise_for :users
-  get 'person/profile', to: 'persons#profile'
-  resources :reviews
-  get 'favorites/:id', to: 'reviews#favorite', as: 'favorited'
-  get 'unfavorites/:id', to: 'reviews#unfavorite', as: 'unfavorited'
+  scope '(:locale)', locale: /en|ru/ do
+    root "reviews#index"
+    devise_for :users
+    get 'person/profile', to: 'persons#profile'
+    resources :reviews
+    get 'favorites/:id', to: 'reviews#favorite', as: 'favorited'
+    get 'unfavorites/:id', to: 'reviews#unfavorite', as: 'unfavorited'
+    get 'set', to: 'application#set_cookie', as: 'cookieset'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
