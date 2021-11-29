0a0c9a398eec0ea21d975f148ea285ec39e3df23
Anton Dydo <dydo00@bk.ru>
2021-11-30 02:13:25 +0300

    some validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,8 @@
 class Review < ApplicationRecord
 	 include PgSearch::Model
+	 validates :category, inclusion: {in: %w(game film book series игра фильм книга сериал), 
+	 	message: I18n.t('reviews.categories')}
+	 validates :authors_grade, inclusion: {in:1..10, message: I18n.t('reviews.grade_error_message')}
 	belongs_to :user
 	has_rich_text :rich_description
 	has_many :favorite_reviews
